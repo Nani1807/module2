a8b163b0c2b175a27f5c9ee9998026b8a7308608
Anastasiia Nuzhna <anastas.nuzhna@gmail.com>
2023-05-23 22:35:10 +0300

    New

---
+++ Module2/Module2/Employee.cs
@@ -0,0 +1,11 @@
+﻿public class Employee
+{
+	public string Name { get; set; }
+	public int Age { get; set; }
+	public string Position { get; set; }
+
+	public void Save()
+	{
+		// Логіка для збереження даних про працівника
+	}
+}
---
+++ Module2/Module2/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http.WebHost;
+using System.Web.Mvc;
+using System.Windows.Forms;
+
+namespace Module2
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee/HireEmployee
+        public ActionResult HireEmployee()
+        {
+            return View();
+        }
+
+        // POST: Employee/HireEmployee
+        [HttpPost]
+        public ActionResult HireEmployee(Employee employee)
+        {
+            // Ваш код для обробки даних працівника
+            // Наприклад, збереження в базі даних
+
+            // При потребі можна встановити повідомлення про успішний прийом на роботу
+            ViewBag.SuccessMessage = "Employee hired successfully.";
+
+            return View();
+        }
+    }
+
+}
